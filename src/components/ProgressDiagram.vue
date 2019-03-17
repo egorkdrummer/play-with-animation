2c932f177fedae9f4bd8656b0732e2634acf38f6
Melnik Igor <egorkdrummer@gmail.com>
2019-03-17 22:21:32 +0200

    update

--- src/components/ProgressDiagram.vue
+++ src/components/ProgressDiagram.vue
@@ -2,7 +2,10 @@
 .progress-diagram
   h4 {{ year }}
   ul
-    li(v-for="(item, index) in monthes") {{ item }}
+    li(v-for="(item, index) in monthes") 
+      template(v-if="item === 0")
+        hr
+      | {{ item }}
 
 </template>
 
@@ -11,7 +14,8 @@ export default {
   data() {
     return {
       year: null,
-      monthes: []
+      monthes: [],
+      days: []
     };
   },
   methods: {
@@ -29,14 +33,35 @@ export default {
       return new Date().getUTCDate();
     },
     createCalendar() {
-      {
+      return {
         //days:
-      }
+      };
+    },
+    daysInMonth(month) {
+      const now = new Date();
+      return new Date(now.getFullYear(), month, 0).getDate();
     }
   },
   created() {
     this.makeCalendar();
     this.getYear();
+  },
+  mounted() {
+    const year = this.getYear();
+    for (let i = 1; i < 13; i++) {
+      this.days.push(this.daysInMonth(i, year));
+    }
+
+    // const manthes = this.days.reduce((prev, curr) => {
+    //   prev.push(new Array(curr).fill(curr))
+    // }, [])
+
+    const monthes = this.days.reduce((acc, curr) => {
+      const month = Array.from(Array(curr).keys());
+      return acc.concat(month);
+    }, []);
+
+    this.monthes = monthes;
   }
 };
 </script>
---
+++ src/views/Animation.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  #animation-page
+    .title {{ title }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Animation page"
+    };
+  }
+};
+</script>
+
